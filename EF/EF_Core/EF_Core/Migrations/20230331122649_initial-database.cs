44bf81d4f5ffa0a7181c22bf83573c16bff68433
Nguyễn Đức Cường <ndcqn02@gmail.com>
2023-03-31 19:32:33 +0700

    EF Core

---
+++ EF/EF_Core/EF_Core/Migrations/20230331122649_initial-database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core.Migrations
+{
+    public partial class initialdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName110 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstMidName110 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnrollmentDate110 = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
